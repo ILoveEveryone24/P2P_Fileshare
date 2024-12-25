6ffafd2cfafe5a7fb93cf5eee559ae0c9cc2e14a
ILoveEveryone24 <ernestascizas2404@gmail.com>
2024-12-25 23:54:12 +0100

    pthread implementation

--- peer.c
+++ peer.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
 
 #define SIG_SERVER_IP "127.0.0.1"
 #define PORT 4444
 
 int main(){
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    if(s < 0){
+        perror("Failed to create a socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in s_addr;
+    socklen_t s_addrlen = sizeof(s_addr);
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SIG_SERVER_IP, &s_addr.sin_addr);
+
+    printf("Connecting");
+
+    int connected = connect(s, (struct sockaddr*)&s_addr, s_addrlen);
+    while(connected < 0){
+        if(errno != EINPROGRESS && errno != ECONNREFUSED){
+            perror("Failed to connect to the server");
+            exit(EXIT_FAILURE);
+        }
+        sleep(1);
+        connected = connect(s, (struct sockaddr*)&s_addr, s_addrlen);
+        printf(".");
+    }
+    printf("\n");
+    printf("CONNECTED\n");
+
+    char request[4096];
+    memset(request, 0, sizeof(request));
+    char response[4096];
+    memset(response, 0, sizeof(response));
+
+    while(1){
+        if(fgets(request, sizeof(request), stdin) != NULL){
+            request[strcspn(request, "\n")] = '\0';
+            send(s, request, sizeof(request), 0);
+        }
+        recv(s, response, sizeof(response), 0);
+        printf("Server: %s\n", response);
+    }
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,5 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+#define PORT 4444
+
+void *handle_client(void *arg){
+    int client_s = *((int *)arg); 
+    char buffer[4096];
+    memset(buffer, 0, sizeof(buffer));
+
+    printf("CLIENT s: %d\n", client_s);
+    
+    int bytes_r; 
+    while((bytes_r = recv(client_s, buffer, sizeof(buffer), 0)) != 0){
+        if(bytes_r < 0){
+            continue;
+        }
+        printf("Client: %s\n", buffer);
+        send(client_s, buffer, sizeof(buffer), 0);
+    }
+}
 
 int main(){
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    if(s < 0){
+        perror("Failed to create a socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in s_addr;
+    socklen_t s_addrlen = sizeof(s_addr);
+
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_port = htons(PORT);
+    s_addr.sin_addr.s_addr = INADDR_ANY;
+    
+    if(bind(s, (struct sockaddr*)&s_addr, s_addrlen) < 0){
+        perror("Failed to bind socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(s, 5) < 0){
+        perror("Failed to listen\n");
+        exit(EXIT_FAILURE);
+    }
+  
+    while(1){
+        int client_s = accept(s, (struct sockaddr*)&s_addr, &s_addrlen);
+        if(client_s < 0){
+            perror("Failed to accept client\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("before CLIENT s: %d\n", client_s);
+        pthread_t tid;
+
+        pthread_create(&tid, NULL, handle_client, (void *)&client_s);
+    }
+
     return 0;
 }
